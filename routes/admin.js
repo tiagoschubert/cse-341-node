dbb7ad64cef0ef48079af9d919105c831821442a
tiagoschubert <51930058+tiagoschubert@users.noreply.github.com>
2021-10-12 14:49:14 -0400

    Add files via upload

---
+++ routes/admin.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const adminController = require('../controllers/admin');
+
+const router = express.Router();
+
+
+//GET
+router.get('/add-product', adminController.getAddProduct);
+
+router.get('/products', adminController.getProducts);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+//POST
+router.post('/add-product', adminController.postAddProduct);
+
+router.post('/edit-product',adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;
